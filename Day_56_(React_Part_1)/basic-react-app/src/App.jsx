020ab0ce4d1ecef1dd42dc69045e5eeeadf71e11
Khushi Mohammd <khushimohamad65@gmail.com>
2025-05-08 23:45:19 +0530

    Day 56 added

---
+++ Day_56_(React_Part_1)/basic-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Title from "./Title.jsx";
+import ProductTab from "./ProductTab.jsx";
+
+function Description() {
+  return <h3>I am the Description!</h3>;
+}
+
+function App() {
+  return <ProductTab />;
+}
+
+export default App;
