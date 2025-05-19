3c0136074b84a88f0460de69058c1f2f6905676a
Khushi Mohammd <khushimohamad65@gmail.com>
2025-05-19 18:05:56 +0530

    Day 59 added

--- Day_59_(React_Part_4)/react-state/src/TodoList.jsx
+++ Day_59_(React_Part_4)/react-state/src/TodoList.jsx
@@ -9,7 +9,8 @@ export default function TodoList() {
     setNewTodo(event.target.value);
   };
 
-  let addNewTodo = () => {
+  let addNewTodo = (event) => {
+    event.preventDefault();
     setTodos((prevTodos) => {
       return [...prevTodos, { task: newTodo, id: uuidv4(), idDone: false }];
     });
@@ -50,10 +51,17 @@ export default function TodoList() {
     setTodos((prevTodos) =>
       prevTodos.map((todo) => {
         if (todo.id === id) {
-          return {
-            ...todo,
-            isDone: true,
-          };
+          if (todo.isDone) {
+            return {
+              ...todo,
+              isDone: false,
+            };
+          } else {
+            return {
+              ...todo,
+              isDone: true,
+            };
+          }
         } else {
           return todo;
         }
@@ -70,31 +78,37 @@ export default function TodoList() {
     // borderRadius: "5px ",
   };
 
+  let btnStyles = {
+    fontSize: "14px",
+    marginLeft: "10px",
+    fontWeight: "400",
+  };
+
   return (
     <div>
       <div>
-        <div>
-          <input
-            type="text"
-            placeholder="Todo"
-            onChange={updateTodoValue}
-            value={newTodo}
-            style={styles}
-          />
-        </div>
+        <form onSubmit={addNewTodo}>
+          <div>
+            <input
+              type="text"
+              placeholder="Todo"
+              onChange={updateTodoValue}
+              value={newTodo}
+              style={styles}
+            />
+          </div>
 
-        <div>
-          <button onClick={addNewTodo} style={styles}>
-            Add
-          </button>
-        </div>
+          <div>
+            <button style={styles}>Add</button>
+          </div>
+        </form>
       </div>
 
       <hr />
       <h2>Todo</h2>
       <ul style={{ textAlign: "start" }}>
         {todos.map((todo) => (
-          <li key={todo.id}>
+          <li key={todo.id} style={{ marginBottom: "10px" }}>
             {todo.isDone ? (
               <span style={{ textDecoration: "line-through" }}>
                 {todo.task}
@@ -103,9 +117,15 @@ export default function TodoList() {
               <span>{todo.task}</span>
             )}
 
-            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-            <button onClick={() => upperCaseOne(todo.id)}>Uppercase</button>
-            <button onClick={() => markAsDone(todo.id)}>Done</button>
+            <button onClick={() => deleteTodo(todo.id)} style={btnStyles}>
+              Delete
+            </button>
+            <button onClick={() => upperCaseOne(todo.id)} style={btnStyles}>
+              Uppercase
+            </button>
+            <button onClick={() => markAsDone(todo.id)} style={btnStyles}>
+              Done
+            </button>
           </li>
         ))}
       </ul>
